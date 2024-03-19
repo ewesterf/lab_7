5956c47b18fd21d89dffe85bc0eaa9c9d917de81
ewesterf <ewesterf@purdue.edu>
2024-03-18 23:05:58 -0400

    why tf does my prof make me use a package that sets off my free version of bitdefender

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -2,10 +2,83 @@
 //
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFPhysics.h>
+
+using namespace std;
+using namespace sf;
+using namespace sfp;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Create our window and world with gracity 0,1
+    RenderWindow window(VideoMode(800, 600), "Bounce");
+    World world(Vector2f(0, 1));
+
+    //Create the ball
+    PhysicsCircle ball;
+    ball.setCenter(Vector2f(400, 300));
+    ball.setRadius(20);
+    world.AddPhysicsBody(ball);
+
+    //Create the floor
+    PhysicsRectangle floor;
+    floor.setSize(Vector2f(800, 20));
+    floor.setCenter(Vector2f(400, 590));
+    floor.setStatic(true);
+    world.AddPhysicsBody(floor);
+
+    //Create the ceiling
+    PhysicsRectangle ceiling;
+    ceiling.setSize(Vector2f(800, 20));
+    ceiling.setCenter(Vector2f(400, 10));
+    ceiling.setStatic(true);
+    world.AddPhysicsBody(ceiling);
+
+    //Left wall
+    PhysicsRectangle leftWall;
+    leftWall.setSize(Vector2f(20, 800));
+    leftWall.setCenter(Vector2f(10, 300));
+    leftWall.setStatic(true);
+    world.AddPhysicsBody(leftWall);
+
+    //Right wall
+    PhysicsRectangle rightWall;
+    rightWall.setSize(Vector2f(20, 800));
+    rightWall.setCenter(Vector2f(790, 300));
+    rightWall.setStatic(true);
+    world.AddPhysicsBody(rightWall);
+    
+
+    int thudCount(0);
+    floor.onCollision = [&thudCount](PhysicsBodyCollisionResult result)
+    {
+            cout << "thud" << thudCount << endl;
+            thudCount++;
+    };
+
+    Clock clock;
+    Time lastTime(clock.getElapsedTime());
+    while (true)
+    {
+        //calculate MS since last frame
+        Time currentTime(clock.getElapsedTime());
+        Time deltaTime(currentTime - lastTime);
+        int deltaTimeMS(deltaTime.asMilliseconds());
+        if (deltaTimeMS > 0)
+        {
+            world.UpdatePhysics(deltaTimeMS);
+            lastTime = currentTime;
+        }
+        
+        window.clear(Color(0, 0, 0));
+        window.draw(ball);
+        window.draw(floor);
+        window.draw(ceiling);
+        window.draw(leftWall);
+        window.draw(rightWall);
+        window.display();
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
